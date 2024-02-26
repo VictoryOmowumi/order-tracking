4af7fc8e6ffc0104f7148ea832ab568f06afaa51
victory balogun <victorybalogun@gmail.com>
2024-02-26 14:36:27 +0100

    updated loading page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-primary-color">
+    <div className="flex flex-col gap-4 items-center justify-center h-screen bg-primary-color">
       <div className="loader"></div>
+      <p className="text-white text-xl font-medium">Loading...</p>
     </div>
   );
 };
